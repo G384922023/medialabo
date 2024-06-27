3b7ed751d87f06f7027b2d173fbe2f33fa947c8c
G384922023 <238492@st.takushoku-u.ac.jp>
2024-06-27 13:41:46 +0900

    project.htmlの活動記録として編集

--- gourmet.js
+++ gourmet.js
@@ -1,114 +1,114 @@
 let data = {
-  "results": {
-    "api_version": "1.26",
-    "results_available": 52,
-    "results_returned": "10",
-    "results_start": 1,
-    "shop": [
+  results: {
+    api_version: "1.26",
+    results_available: 52,
+    results_returned: "10",
+    results_start: 1,
+    shop: [
       {
-        "access": "京王八王子駅を背にし右手に見えるローソンの隣のビル、ホテルザ・ビーの2階です。",
-        "address": "東京都八王子市明神町４-6-12　ホテル・ザ・ビー八王子（旧八王子プラザホテル）2F",
-        "band": "可",
-        "barrier_free": "なし",
-        "budget": {
-          "average": "ディナー3000円",
-          "code": "B003",
-          "name": "3001～4000円"
+        access: "京王八王子駅を背にし右手に見えるローソンの隣のビル、ホテルザ・ビーの2階です。",
+        address: "東京都八王子市明神町４-6-12　ホテル・ザ・ビー八王子（旧八王子プラザホテル）2F",
+        band: "可",
+        barrier_free: "なし",
+        budget: {
+          average: "ディナー3000円",
+          code: "B003",
+          name: "3001～4000円"
         },
-        "budget_memo": "",
-        "capacity": 100,
-        "card": "利用可",
-        "catch": "【サプライズ演出有】 結婚パーティー受付中",
-        "charter": "貸切可 ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア！50型モニター・スポットライト等充実",
-        "child": "お子様連れOK",
-        "close": "不定休有り。店休時は店長携帯電話09035324825まで問い合わせください♪",
-        "coupon_urls": {
-          "pc": "https://www.hotpepper.jp/strJ000989843/map/?vos=nhppalsa000016",
-          "sp": "https://www.hotpepper.jp/strJ000989843/scoupon/?vos=nhppalsa000016"
+        budget_memo: "",
+        capacity: 100,
+        card: "利用可",
+        catch: "【サプライズ演出有】 結婚パーティー受付中",
+        charter: "貸切可 ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア！50型モニター・スポットライト等充実",
+        child: "お子様連れOK",
+        close: "不定休有り。店休時は店長携帯電話09035324825まで問い合わせください♪",
+        coupon_url: {
+          pc: "https://www.hotpepper.jp/strJ000989843/map/?vos=nhppalsa000016",
+          sp: "https://www.hotpepper.jp/strJ000989843/scoupon/?vos=nhppalsa000016"
         },
-        "course": "あり",
-        "english": "なし",
-        "free_drink": "あり",
-        "free_food": "なし",
-        "genre": {
-          "catch": "大人の社交場非日常を味わうダイニング",
-          "code": "G001",
-          "name": "居酒屋"
+        course: "あり",
+        english: "なし",
+        free_drink: "あり",
+        free_food: "なし",
+        genre: {
+          catch: "大人の社交場非日常を味わうダイニング",
+          code: "G001",
+          name: "居酒屋"
         },
-        "horigotatsu": "なし",
-        "id": "J000989843",
-        "karaoke": "あり",
-        "ktai_coupon": 0,
-        "large_area": {
-          "code": "Z011",
-          "name": "東京"
+        horigotatsu: "なし",
+        id: "J000989843",
+        karaoke: "あり",
+        ktai_coupon: 0,
+        large_area: {
+          code: "Z011",
+          name: "東京"
         },
-        "large_service_area": {
-          "code": "SS10",
-          "name": "関東"
+        large_service_area: {
+          code: "SS10",
+          name: "関東"
         },
-        "lat": 35.6585460152,
-        "lng": 139.34327231,
-        "logo_image": "https://imgfp.hotp.jp/IMGH/21/04/P038512104/P038512104_69.jpg",
-        "lunch": "なし",
-        "middle_area": {
-          "code": "Y110",
-          "name": "八王子・立川"
+        lat: 35.6585460152,
+        lng: 139.34327231,
+        logo_image: "https://imgfp.hotp.jp/IMGH/21/04/P038512104/P038512104_69.jpg",
+        lunch: "なし",
+        middle_area: {
+          code: "Y110",
+          name: "八王子・立川"
         },
-        "midnight": "営業していない",
-        "mobile_access": "京王八王子駅1分/JR八王子駅北口5分",
-        "name": "バグダッドカフェ Bagdadcafe/モータウン MOTOWN",
-        "name_kana": "ばぐだっどかふぇもーたうんはちおうじ",
-        "non_smoking": "禁煙席なし",
-        "open": "月～日、祝日、祝前日: 17:00～21:00 （料理L.O. 20:00 ドリンクL.O. 20:00）",
-        "other_memo": "ステージ・マイク・カラオケ等",
-        "parking": "なし",
-        "party_capacity": 250,
-        "pet": "不可",
-        "photo": {
-          "mobile": {
-            "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_100.jpg"
+        midnight: "営業していない",
+        mobile_access: "京王八王子駅1分/JR八王子駅北口5分",
+        name: "バグダッドカフェ Bagdadcafe/モータウン MOTOWN",
+        name_kana: "ばぐだっどかふぇもーたうんはちおうじ",
+        non_smoking: "禁煙席なし",
+        open: "月～日、祝日、祝前日: 17:00～21:00 （料理L.O. 20:00 ドリンクL.O. 20:00）",
+        other_memo: "ステージ・マイク・カラオケ等",
+        parking: "なし",
+        party_capacity: 250,
+        pet: "不可",
+        photo: {
+          mobile: {
+            l: "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
+            s: "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_100.jpg"
           },
-          "pc": {
-            "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_238.jpg",
-            "m": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_58_s.jpg"
+          pc: {
+            l: "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_238.jpg",
+            m: "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
+            s: "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_58_s.jpg"
           }
         },
-        "private_room": "あり ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア",
-        "service_area": {
-          "code": "SA11",
-          "name": "東京"
+        private_room: "あり ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア",
+        service_area: {
+          code: "SA11",
+          name: "東京"
         },
-        "shop_detail_memo": "ご質問、ご要望はお気軽にお問い合わせください♪",
-        "show": "あり",
-        "small_area": {
-          "code": "X220",
-          "name": "八王子"
+        shop_detail_memo: "ご質問、ご要望はお気軽にお問い合わせください♪",
+        show: "あり",
+        small_area: {
+          code: "X220",
+          name: "八王子"
         },
-        "station_name": "京王八王子",
-        "sub_genre": {
-          "code": "G002",
-          "name": "ダイニングバー・バル"
+        station_name: "京王八王子",
+        sub_genre: {
+          code: "G002",
+          name: "ダイニングバー・バル"
         },
-        "tatami": "なし",
-        "tv": "あり",
-        "urls": {
-          "pc": "https://www.hotpepper.jp/strJ000989843/?vos=nhppalsa000016"
+        tatami: "なし",
+        tv: "あり",
+        urls: {
+          pc: "https://www.hotpepper.jp/strJ000989843/?vos=nhppalsa000016"
         },
-        "wedding": "大歓迎！ステージ・マイク・音響・映像等、設備充実！！最大200名様まで是非どうぞ★",
-        "wifi": "あり"
+        wedding: "大歓迎！ステージ・マイク・音響・映像等、設備充実！！最大200名様まで是非どうぞ★",
+        wifi: "あり"
       },
       {
-        "access": "ＪＲ八王子駅北口徒歩1分/京王八王子駅徒歩3分",
-        "address": "東京都八王子市東町12-14",
-        "band": "不可",
-        "barrier_free": "あり ：スタッフがお手伝いさせて頂きます。ご不明な点等お気軽にお声掛け下さい。事前のお問い合わせも◎",
-        "budget": {
-          "average": "2,000円(通常平均) 3,000円(宴会平均) ",
-          "code": "B002",
-          "name": "2001～3000円"
+        access: "ＪＲ八王子駅北口徒歩1分/京王八王子駅徒歩3分",
+        address: "東京都八王子市東町12-14",
+        band: "不可",
+        barrier_free: "あり ：スタッフがお手伝いさせて頂きます。ご不明な点等お気軽にお声掛け下さい。事前のお問い合わせも◎",
+        budget: {
+          average: "2,000円(通常平均) 3,000円(宴会平均) ",
+          code: "B002",
+          name: "2001～3000円"
         },
         "budget_memo": "",
         "capacity": 100,
@@ -200,4 +200,6 @@ let data = {
 };
 
 /////////// 課題3-2 ここからプログラムを書こう
+
+console.log(data);
 
